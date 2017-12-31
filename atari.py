36354b52a459dd6353a7d63e1a29a236fe78b45a
scey26 <scey26@naver.com>
2017-12-31 14:27:49 +0900

    changyeop new_1231_2pm

--- atari.py
+++ atari.py
@@ -1,4 +1,3 @@
-from random import randrange as rand
 import pygame, sys
 
 cell_size = 5
@@ -21,6 +20,8 @@ colors = [
 
 class atari(object):
     def __init__(self):
+        pygame.init()
+        pygame.key.set_repeat()
         self.width = cell_size * cols
         self.height = cell_size * rows
         self.screen = pygame.display.set_mode((self.width, self.height))
@@ -28,7 +29,7 @@ class atari(object):
             pygame.font.get_default_font(), 12
         )
         self.ball = [1]
-        self.plate = [3 for i in range(4)]
+        self.plate = [[3 for i in range(4)]]
         self.ball_speed = [0, 0]
         self.ball_x = 0
         self.ball_y = 0
@@ -45,10 +46,13 @@ class atari(object):
         self.board += [[0 for x in range(cols)] for y in range(rows - brick_start - layer)]
         # default board
 
+
+
     def join_matrixes(self, mat1, mat2, mat1_x, mat1_y):
         for cy, row in enumerate(mat1):
-            for cx, val in enumerate(mat1):
+            for cx, val in enumerate(row):
                 mat2[mat1_y+cy][mat1_x+cx] = val
+
 
 
     def new_plate(self):
@@ -56,7 +60,11 @@ class atari(object):
         self.plate_y = rows-3
         self.join_matrixes(self.plate, self.board, self.plate_x, self.plate_y)
 
+
     def move_plate(self, delta_x):
+        for i in range(self.plate_x, self.plate_x + len(self.plate[0]), 1):
+            self.board[self.plate_y][i] = 0
+
         new_x = self.plate_x + delta_x
 
         if new_x < 0:
@@ -64,15 +72,51 @@ class atari(object):
         if new_x + len(self.plate) > cols :
             new_x = cols - len(self.plate)
 
-    def move_ball(self):
-        self.collision()
-        self.ball_x += self.ball_speed[0]
-        self.ball_y += self.ball_speed[1]
-        self.board[ball_x][ball_y] = 2
-        if ball_y == rows :
-            self.gameover = True
+        self.plate_x = new_x
+
+        self.join_matrixes(self.plate, self.board, self.plate_x, self.plate_y)
 
 
+#     def move_ball(self):
+#         self.collision()
+#         self.board[self.ball_x][self.ball_y] = 0
+#         self.ball_x += self.ball_speed[0]
+#         self.ball_y += self.ball_speed[1]
+#         self.board[self.ball_x][self.ball_y] = 2
+#         if self.ball_y == rows :
+#             self.gameover = True
+#
+#     def collision(self):
+#         # when collide to wall
+#         if ([self.ball_x, self.ball_y] == [0 , 0])\
+#                 or ([self.ball_x, self.ball_y] == [cols, 0]):
+#             self.ball_speed = [-self.ball_speed[0], -self.ball_speed[1]]
+#         elif self.ball_x == 0 or self.ball == cols:
+#             self.ball_speed = [-self.ball_speed[0], self.ball_speed[1]]
+#         elif self.ball_y == 0:
+#             self.ball_speed = [self.ball_speed[0], -self.ball_speed[1]]
+#         elif self.ball_y == rows:
+#             return
+#         # when collide to brick or plate
+#         else:
+#             check_corner = True
+#             for i, off in enumerate([(0, -1), (1, 0), (0, 1), (-1, 0)]):
+#                 x = self.ball_x + off[0]
+#                 y = self.ball_y + off[1]
+#                 val = self.board[x][y]
+#                 if not val:
+#                     check_corner = False
+#                     if i == 0 or i == 3:
+#                         self.ball_speed = [self.ball_speed[0], -self.ball_speed[1]]
+#                     else:
+#                         self.ball_speed = [-self.ball_speed[0], self.ball_speed[1]]
+#                 # delete brick
+#             if check_corner:
+#                 for i, off in enumerate([(-1, -1), (1, -1), (1, 1), (-1, 1)]):
+#                     if not self.board[x][y]:
+#                         self.ball_speed = [-self.ball_speed[0], -self.ball_speed[1]]
+#                         break
+#
     def center_msg(self, msg):
         for i, line in enumerate(msg.splitlines()):
             msg_image = self.default_font.render(line, False,
@@ -85,47 +129,73 @@ class atari(object):
             self.screen.blit(msg_image, (
                 self.width // 2 - msgim_center_x,
                 self.height // 2 - msgim_center_y + i*22))
-    def draw_matrix(self, matrix, offset):
-        off_x, off_y = offset
-        for y, row in enumerate(matrix):
-            for x, val in enumerate(row):
-                if val:
-                    pygame.draw.rect(
-                        self.screen,
-                        colors[val],
-                        pygame.Rect(
-                            (off_x + x) *
-                            cell_size,
-                            (off_y+y) *
-                            cell_size,
-                            cell_size,
-                            cell_size), 0)
 
+game = atari()
+game.new_board()
+game.new_plate()
+game.move_plate(3)
+game.screen.fill((0, 0, 0))
+game.center_msg("horse")
+pygame.display.update()
+print(1)
 
-    def run(self):
-        dont_burn_my_cpu = pygame.time.Clock()
-        while True:
-            # display update
-            self.screen.fill((0, 0, 0))
-            if self.gameover:
-                self.center_msg("""Game Over Horse! \n
-                Your score : %d Press space to continue""" % self.score)
-            else:
-                if self.paused:
-                    self.center_msg("Paused horse!")
-                else:
-                    self.draw_matrix(self.board, (0,0))
-                    self.draw_matrix()
-                    pass
-
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                # do it
-                pass
-
-            dont_burn_my_cpu(maxfps)
-
-
-
-
+#     def draw_matrix(self, matrix, offset):
+#         off_x, off_y = offset
+#         for y, row in enumerate(matrix):
+#             for x, val in enumerate(row):
+#                 if val:
+#                     pygame.draw.rect(
+#                         self.screen,
+#                         colors[val],
+#                         pygame.Rect(
+#                             (off_x + x) *
+#                             cell_size,
+#                             (off_y+y) *
+#                             cell_size,
+#                             cell_size,
+#                             cell_size), 0)
+#
+#
+#     def run(self):
+#         dont_burn_my_cpu = pygame.time.Clock()
+#         key_actions = {
+#             'ESCAPE' : self.quit,
+#             'LEFT' : lambda: self.move_plate(-1),
+#             'RIGHT' : lambda: self.move_plate(+1),
+#             'p' : self.toggle_pause,
+#             'SPACE' : self.start_game
+#         }
+#         while True:
+#             # display update
+#             self.screen.fill((0, 0, 0))
+#             if self.gameover:
+#                 self.center_msg("""Game Over Horse! \n
+#                 Your score : %d Press space to continue""" % self.score)
+#             else:
+#                 if self.paused:
+#                     self.center_msg("Paused horse!")
+#                 else:
+#                     self.draw_matrix(self.board, (0,0))
+#                     pass
+#
+#             pygame.display.update()
+#
+#             for event in pygame.event.get():
+#                 if event.type == pygame.USERVENT + 1:
+#                     self.drop(False)
+#                 elif event.type == pygame.QUIT:
+#                     self.quit()
+#                 elif event.type == pygame.KEYDOWN
+#                     for key in key_actions:
+#                         if event.key == eval("pygame.K_"
+#                                              + key):
+#                             key_actions[key]()
+#
+#
+#             dont_burn_my_cpu(maxfps)
+#
+# if __name__ == '__main__':
+#     App = atari()
+#     App.run()
+#
+#
